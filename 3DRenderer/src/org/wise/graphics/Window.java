be5593e1a12670e6540ae5bab2b8e5727e931668
Daniel <daniel.seidel.work@gmail.com>
2018-11-09 21:28:53 +0100

    wrote a few keylistener lines for wiedma

--- 3DRenderer/src/org/wise/graphics/Window.java
+++ 3DRenderer/src/org/wise/graphics/Window.java
@@ -1,13 +1,16 @@
 package org.wise.graphics;
 
 import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.geom.Point2D;
 
 import javax.swing.JFrame;
 
+import org.wise.math.Vector;
 import org.wise.world.WorldSpace;
 
-public class Window extends JFrame { private static final long serialVersionUID = 5325798843479656003L;
+public class Window extends JFrame implements KeyListener{ private static final long serialVersionUID = 5325798843479656003L;
 	
 	private DrawComp dc;
 	
@@ -18,6 +21,7 @@ public class Window extends JFrame { private static final long serialVersionUID 
 		
 		this.camera = camera;
 		
+		this.addKeyListener(this);
 		this.setTitle("The wise 3D Renderer");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -41,6 +45,36 @@ public class Window extends JFrame { private static final long serialVersionUID 
 	
 	public Camera getCamera() {
 		return this.camera;
+	}
+
+	public void keyTyped(KeyEvent e) {
+	}
+
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_A) {
+			camera.getPosition().increment(new Vector(-1, 0, 0));
+		}
+		if(e.getKeyCode() == KeyEvent.VK_D) {
+			camera.getPosition().increment(new Vector(1, 0, 0));
+		}
+		if(e.getKeyCode() == KeyEvent.VK_SHIFT) {
+			camera.getPosition().increment(new Vector(0, 1, 0));
+		}
+		if(e.getKeyCode() == KeyEvent.VK_CONTROL) {
+			camera.getPosition().increment(new Vector(0, -1, 0));
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_W) {
+			camera.getPosition().increment(new Vector(0, 0, -1));
+		}
+		if(e.getKeyCode() == KeyEvent.VK_S) {
+			camera.getPosition().increment(new Vector(0, 0, 1));
+		}
+		
+		repaint();
+	}
+
+	public void keyReleased(KeyEvent e) {
 	}
 	
 }
